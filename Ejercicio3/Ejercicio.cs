6e21d3cd9fcd0a47de1ac22ea3bd368ee70493cc
Hugo Mendez <hugo3011mendez@gmail.com>
2020-11-09 20:31:17 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Ejercicio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Ejercicio
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            // Al ejecutar el programa se debe inicializar la colección automátcamente al menos con 3 ttulos (usa una directva de compilación para controlar esto).
+
+
+
+            bool repetir = true;
+            while (repetir)
+            {
+                Console.WriteLine("1 - Insertar nuevo videojuego");
+                Console.WriteLine("2 - Eliminar videojuegos");
+                Console.WriteLine("3 - Visualizar toda la lista de videojuegos");
+                Console.WriteLine("4 - Visualización de videojuegos por estilo");
+                Console.WriteLine("5 - Modificar videojuego ");
+                Console.WriteLine("6 - Salir");
+                Console.WriteLine();
+                Console.WriteLine("Cuál eliges?");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+                    case 6:
+                        repetir = false;
+                        Console.WriteLine("Hasta luego!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Esa opción no existe, repite");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio3/Videojuego.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Videojuego
+    {
+        string titulo { set; get; }
+        int anho { set; get; }
+        enum Estilo
+        {
+            Arcade,
+            Videoaventura,
+            Shootemup,
+            Estrategia,
+            Deportivo, 
+            Survival,
+            Terror,
+            Sandbox,
+            RPG,
+            MOBA
+        }
+    }
+}
